e56baf8a45b0517a5e8fd485264c2057993e3b3b
Carlos W. Gama <carloswgama@gmail.com>
2019-02-12 15:19:19 -0300

    Adicionado Iteração

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  login: {email:string, senha:string} = {email: 'carlos', senha: '123456'};
+  msg;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clicou() {
+    if (this.login.email == 'teste@teste.com' && this.login.senha == '123456')
+      this.msg = "Sucesso";
+    else
+      this.msg = "Email ou senha incorreta";
   }
 
 }
