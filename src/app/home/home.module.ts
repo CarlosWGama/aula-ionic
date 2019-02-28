dcea44e88309f0d126a70f49073b78d2f570154d
Carlos W. Gama <carloswgama@gmail.com>
2019-02-27 21:41:12 -0300

    'Componente

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { PrazoComponent } from './prazo/prazo.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, PrazoComponent]
 })
 export class HomePageModule {}
