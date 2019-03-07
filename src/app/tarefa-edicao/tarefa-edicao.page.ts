232e6c2eec59670b621234af0d2ebd80965c5605
Carlos W. Gama <carloswgama@gmail.com>
2019-03-07 15:24:48 -0300

    Adicioandno camera

--- src/app/tarefa-edicao/tarefa-edicao.page.ts
+++ src/app/tarefa-edicao/tarefa-edicao.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Tarefa } from '../models/tarefa';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tarefa-edicao',
@@ -13,8 +14,9 @@ export class TarefaEdicaoPage implements OnInit {
   tarefa: Tarefa;
 
 
-  constructor(private activitedRouted:ActivatedRoute, private router: Router, private toastController:ToastController) {   
-  }
+  constructor(private activitedRouted:ActivatedRoute, 
+              private router: Router, private toastController:ToastController,
+              private camera:Camera) { }
 
   ngOnInit() {
     if (this.activitedRouted.snapshot.params['id'])
@@ -24,7 +26,18 @@ export class TarefaEdicaoPage implements OnInit {
   }
 
   tirarFoto() {
-    this.tarefa.imagem = "/assets/imgs/camera_on.png";
+    this.camera.getPicture({
+      cameraDirection: this.camera.Direction.BACK,
+      allowEdit: true,
+      quality: 100,
+      saveToPhotoAlbum: false,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(foto => {
+      this.tarefa.imagem = 'data:image/jpeg;base64,'+foto;
+    });
   }
 
   salvar() {
